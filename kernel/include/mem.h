ec181ffc41e57c25978c666c9b016845fe65049d
Pepa3 <svobodajosef117@gmail.com>
2022-12-14 21:03:41 +0100

    memory

--- kernel/include/mem.h
+++
@@ -1,4 +0,0 @@
-#include <stdint.h>
-
-void memory_copy(uint8_t *source, uint8_t *destination, int bytes);
-void memory_set(uint8_t *destination, uint8_t value, uint32_t len);
---
+++ kernel/include/memory.h
@@ -0,0 +1,28 @@
+#ifndef MEMORY_H_INC
+#define MEMORY_H_INC
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct {
+	uint8_t status;
+	uint32_t size;
+} alloc_t;
+
+void mm_init(uint32_t kernel_end);
+void mm_print_out();
+
+void paging_init();
+void paging_map_virtual_to_phys(uint32_t virt, uint32_t phys);
+
+char* pmalloc(size_t size); /* page aligned alloc */
+char* malloc(size_t size);
+void free(void *mem);
+
+
+void memcpy(uint8_t *destination, uint8_t *source, int bytes);
+void memset(uint8_t *destination, uint8_t value, uint32_t len);
+//void* memcpy(const void* dest, const void* src, size_t num );
+//void* memset (void * ptr, int value, size_t num );
+
+#endif
