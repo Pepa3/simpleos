71aa5ee0d7fc5bc5b9d0bb05e2837c02a861e186
Pepa3 <svobodajosef117@gmail.com>
2022-12-15 20:00:12 +0100

    remove printf

--- kernel/std/memory.c
+++ kernel/std/memory.c
@@ -124,7 +124,7 @@ uint8_t* malloc(size_t size)
 	last_alloc += size;
 	last_alloc += sizeof(alloc_t);
 	last_alloc += 4;
-	printf("Allocated %d bytes from 0x%x to 0x%x\n", size, (uint32_t)alloc + sizeof(alloc_t), last_alloc);
+	//printf("Allocated %d bytes from 0x%x to 0x%x\n", size, (uint32_t)alloc + sizeof(alloc_t), last_alloc);
 	memory_used += size + 4 + sizeof(alloc_t);
 	memset((char *)((uint32_t)alloc + sizeof(alloc_t)), 0, size);
 	return (uint8_t *)((uint32_t)alloc + sizeof(alloc_t));
