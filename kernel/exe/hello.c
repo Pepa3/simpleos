4bf2ab15890df65165814ceda6099674e6cdd9e7
Pepa3 <svobodajosef117@gmail.com>
2022-12-17 20:18:19 +0100

    syscalls wip, user-app wip

---
+++ kernel/exe/hello.c
@@ -0,0 +1,10 @@
+static char *text = "Hello, world! SimpleOS and Linux cross-compatibility!\n";
+void main()
+{
+	/* syscall 4: write()      eax=4  ebx=file ecx=buffer edx=size RET: ecx=size*/
+	asm volatile("int $0x80": :"a"(4), "b"(0), "c"(text), "d"(55));
+	/* syscall 1: exit()       eax=1 ebx=err */
+	asm volatile("xor %eax, %eax\n");
+	asm volatile("int $0x80": :"a"(1), "b"(0));
+	while(1);
+}
