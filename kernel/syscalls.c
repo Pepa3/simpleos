6ab1623fc35559350acd1a70e664b0c3dd6e359b
Pepa3 <svobodajosef117@gmail.com>
2022-12-21 20:05:26 +0100

    too soon for apps, TODO: syscalls

--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -9,129 +9,29 @@
 
 #define p_pid() 2
 
-void syscall()
-{
-	asm volatile("add $0x2c, %esp");
-	asm volatile("pusha");
-	int eax = 0;
-	int ebx = 0;
-	int ecx = 0;
-	int edx = 0;
-	asm volatile("movl %%eax, %0":"=m"(eax));
-	asm volatile("movl %%ebx, %0":"=m"(ebx));
-	asm volatile("movl %%ecx, %0":"=m"(ecx));
-	asm volatile("movl %%edx, %0":"=m"(edx));
-	printf("SYSCALL! a: 0x%x b: 0x%x c:0x%x ", eax, ebx, ecx);
-    goto end;
-	//printf("d: %d\n", edx);
-	switch(eax)
+void syscall(registers_t r){
+	/*
+	* eax=0
+	*/
+	switch(r.ebx)
 	{
-		case 0: // syscall 0: restart()
+		case 0: // restart
 			break;
-		case 1: // syscall 1: exit() eax=1 ebx=err
-			//_kill();
-			for(;;);
+		case 1: // exit
+			goto end;
 			break;
-		case 2: // syscall 2: fork() eax = 2 ebx=pt_regs (?)
+		case 2: // read stdin ecx = 
 			break;
-		case 3: // syscall 3: read() eax=3 ebx=file ecx=buf edx=size
-			switch(ebx)
-			{
-				case 1: // stdout
-					break;
-				case 0: // stdin
-					for(int i = 0;i < edx; i++)
-					{
-						char c = 0;
-						while(!(c = keyboard_get_key()));
-						*((uint8_t *)(ecx + i)) = c;
-						printf("%c", c);
-						//printf("(0x%x) %c\n", ((uint8_t *)(ecx + i)), *((uint8_t *)(ecx + i)));
-						if(c == '\n')
-						{
-							edx = i;
-							break;
-						}
-					}
-					break;
-				case 2: // stderr
-					break;
-				default:
-					break;
-			}
-			asm volatile("movl %%eax, %%edx": :"a"(edx*4));
+		case 3:	// write stdout ecx = str*
+			printf("%s",r.ecx);
 			break;
-		case 4: // syscall 4: write() eax=4 ebx=file ecx=buffer edx=size RET: ecx=size
-			switch(ebx)
-			{
-				case 0: // STDOUT
-					;
-					char *str = (char*)malloc(edx + 1);
-					memcpy((uint8_t*)str, ecx, edx);
-					str[edx + 1] = 0;
-					printf("%s", str);
-					free(str);
-					break;
-				case 1: // stdin
-					// Can't write to stdin!
-					asm volatile("movl $0, %ecx");
-					break;
-				case 2: // stderr
-					printf("ERR: %s", ecx);
-					break;
-				default:
-					// To find descriptor substracr 4 from id
-					asm volatile("movl $0, %ecx");
-					break;
-			}
-			break;
-		/*case 5: // syscall 5: open() eax=5 ebx=file ecx=flags  edx=mode RET: eax=file
-			//asm volatile("movl $0, %eax");
-			;
-			uint32_t desc = 0xffffffff;
-			switch(ebx)
-			{
-				case 0: // stdout 
-					desc = 0;
-					break;
-				case 1: // stdin 
-					desc = 1;
-					break;
-				case 2: // stderr 
-					desc = 2;
-					break;
-			}
-			// Add to open list 
-			PROCESS *c = p_proc();
-			if(c->num_open_files >= MAX_OPEN_FILES)
-			{
-				desc = 0xffffffff; // @temp @todo
-			} else {
-				if(desc == 0xffffffff)
-				{
-					if(vfs_exist_in_dir("", (char *)ebx))
-					{
-						char *openbuf = (char *)malloc(strlen((char *)ebx) + 1);
-						memcpy(openbuf, (char *) ebx, strlen((char *)ebx) + 1);
-						c->open_files[c->num_open_files] = openbuf;
-						desc = c->num_open_files + 4;
-						c->num_open_files ++;
-					} else {
-						desc = 0xffffffff;
-					}
-				}
-			}
-			asm volatile("movl %%ebx, %%eax": :"b"(desc));
-			break;*/
-		case 0x14: // syscall 0x14: getpid() eax=0x14 RET: eax=pid
-			asm volatile("movl %%ebx, %%eax": :"b"(p_pid()));
+		case 4:	// write stderr ecx = str*
+			printf("ERR: %s",r.ecx);
 			break;
 		default:
 			break;
 	}
-    end:
-    asm volatile("popa");
-	asm volatile("iret");
+	end:;
 }
 
 void syscall_init()
